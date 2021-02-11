c6d88c06016ebd08619ff43c2f047806b3d9a388
WhoIAm-Benjamin <76595934+WhoIAm-Benjamin@users.noreply.github.com>
2021-02-11 17:35:55 +0300

    Add files via upload

---
+++ Most_need_distributivs.bat
@@ -0,0 +1,93 @@
+rem самые нужные дистрибутивы для работы с Python программами с GUI и без него
+
+@ECHO OFF
+
+cls
+
+python -m pip install --upgrade pip
+rem pip install --upgrade pip
+pip install wheel
+
+pip install autoit
+pip install bs4
+rem pip install scipy
+pip install colorama
+rem pip install confidence
+rem pip install django rem in virtual environment
+rem pip install fake_useragent
+pip install flake8
+pip install keyboard
+rem pip install kivy
+rem pip install matplotlib
+pip install mouse
+rem pip install numpy
+rem pip install pandas
+pip install pillow
+pip install PyAESCrypt
+pip install pyinstaller
+pip install pynput
+pip install PySide2
+rem pip install sklearn
+rem pip install statistics
+pip install pyqt5_tools
+pip install requests
+rem pip install scikit-learn
+rem pip install telebot
+pip install urllib3 
+pip install selenium
+pip install pywinauto
+rem pip install
+rem pip install
+rem pip install 
+rem pip install
+rem pip install
+rem pip install
+rem pip install
+rem pip install 
+rem pip install
+rem pip install
+rem pip install
+rem pip install
+rem pip install 
+rem pip install
+rem pip install
+rem pip install
+rem pip install
+rem pip install 
+rem pip install
+rem pip install
+rem pip install
+rem pip install
+rem pip install 
+rem pip install
+rem pip install
+rem pip install
+rem pip install
+rem pip install 
+rem pip install
+rem pip install
+rem pip install
+rem pip install
+rem pip install 
+rem pip install
+rem pip install
+rem pip install
+rem pip install
+rem pip install 
+rem pip install
+rem pip install
+rem pip install
+rem pip install
+rem pip install 
+rem pip install
+rem pip install
+rem pip install
+rem pip install
+rem pip install 
+rem pip install
+rem pip install
+rem pip install
+rem pip install
+rem pip install 
+pip list
+set/p"cho=>"
